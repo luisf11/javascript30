804979375a5346f26cd319f54446861bf462e542
luisf11 <luisfernandop1110@gmail.com>
2018-01-09 22:56:27 -0400

    Update main index

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const challenges = [
     path: "./01 - JavaScript Drum Kit/index.html"
   },
   { name: " 02 - JS and CSS Clock", path: "./02 - JS and CSS Clock/index.html" },
-  { name: " 03 - CSS Variables", path: "./03 - CSS Variables/index.html" }
+  { name: " 03 - CSS Variables", path: "./03 - CSS Variables/index.html" },
+  { name: " 04 - Array Cardio Day 1", path: "./04 - Array Cardio Day 1/index.html" }
 ];
 
 const card = `
